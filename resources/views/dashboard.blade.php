c890ffdcd61a7abdabe3e7ed69cdc009286cfef4
Morella92 <morena.piem@outlook.com>
2023-05-24 10:52:18 +0200

    FEAT button in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,9 @@
         {{-- {{ __('Dashboard') }}  --}}
         Pannello di amministrazione dell'Insegnante di {{  Auth::user()->name }}
     </h2>
+
+    <a href="{{ route('teachers.create')}}" class="btn btn-warning my-3">Crea il tuo profilo</a>
+    
        {{-- VISTA TEACHER LIST INDEX --}}
     <div class="card" style="width: 18rem;">
         <img src="{{ asset('/img/varie/teachers.jpeg') }}" class="card-img-top" alt="..."
@@ -13,7 +16,7 @@
       <h5 class="card-title text-uppercase text-center fw-bold text-danger mt-3">lista insegnanti</h5>
 
       <p class="card-text">Clicca sul bottone per visualizzare ls lista completa dei INSEGNANTI.</p>
-      <a href="{{ route('teachers.index')}}" class="btn btn-primary text-uppercase fw-bolder">Mostra lista insegnanti</a>
+      <a href="{{ route('teachers.index') }}" class="btn btn-primary text-uppercase fw-bolder">Mostra lista insegnanti</a>
     </div>
   </div>
 
@@ -21,22 +24,5 @@
     <div>
         
     </div>
-    {{-- <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-                    {{ __('You are logged in!') }}
-                </div>
-                
-            </div>
-        </div>
-    </div> --}}
 </div>
 @endsection
