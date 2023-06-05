ccf08d8d8a8b6f9dee0c139d010a0689957c2159
Cianuro-83 <buzzeosimone@yahoo.com>
2023-06-05 04:54:27 +0200

    FEAT: fake date boot x messaggi e recensioni

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,11 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Message extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (is_null($model->created_at)) {
+                $startDate = Carbon::create(2020, 1, 1);
+                $endDate = Carbon::create(2023, 6, 5);
+                $currentDate = Carbon::now();
+
+                if ($currentDate->greaterThanOrEqualTo(Carbon::create(2023, 6, 6))) {
+                    $model->created_at = $currentDate;
+                } else {
+                    $randomDate = Carbon::createFromTimestamp(mt_rand($startDate->timestamp, $endDate->timestamp));
+                    $model->created_at = $randomDate;
+                }
+            }
+        });
+    
+    }
+
+
+
+
+
+
     protected $fillable = [
         'text',
         'ui_name',
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -11,6 +11,27 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (is_null($model->created_at)) {
+                $startDate = Carbon::create(2020, 1, 1);
+                $endDate = Carbon::create(2023, 6, 5);
+                $currentDate = Carbon::now();
+
+                if ($currentDate->greaterThanOrEqualTo(Carbon::create(2023, 6, 6))) {
+                    $model->created_at = $currentDate;
+                } else {
+                    $randomDate = Carbon::createFromTimestamp(mt_rand($startDate->timestamp, $endDate->timestamp));
+                    $model->created_at = $randomDate;
+                }
+            }
+        });
+    
+    }
+
     protected $fillable = [
         'userReview',
         'review',
